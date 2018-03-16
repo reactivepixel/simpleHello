010d2feeed1c9797f08d009413dff11376348952
Chris Chapman <Chapman@apextion.com>
2018-03-16 11:43:44 -0400

    hook is firing

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
+app.post('/webhook', (req, res) => {
+  res.send('GOT IT');
+  console.log(req.body);
+});
+
+const port = process.env.PORT || 3333;
 
 app.listen(port, ()=>{
   console.log('server is active on port ' + port);
